092478d8d9217b390d94f7ae47158b57b752e523
alvan akin <akin0046@algonquinlive.com>
2023-11-16 16:52:40 -0500

    adds javascript funtions

---
+++ javascript.js
@@ -0,0 +1,23 @@
+function setTitle(title) {
+    document.getElementById('page-title').textContent = title;
+}
+
+function setDescription(description) {
+    document.getElementById('page-description').textContent = description;
+}
+
+function setBackgroundColor(color) {
+    document.body.style.backgroundColor = color;
+}
+
+function setFontColor(color) {
+    document.body.style.color = color;
+}
+
+function setTheme(themeName) {
+    document.body.classList.remove('dark-theme', 'light-theme');
+    if (themeName === 'dark' || themeName === 'light') {
+        document.body.classList.add(`${themeName}-theme`);
+    }
+}
+
